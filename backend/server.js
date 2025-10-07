aa0cdce75cc3f02e77c42e15eaf73dc2ca27c185
Vendel00 <115153559+Vendel00@users.noreply.github.com>
2025-10-07 13:13:23 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,31 @@ app.use(cors());
         res.send("Fut a backend!");
 })
 
+app.get("/hettorpefogado", (req, res) => {
+    const sql = "SELECT DISTINCT sznev, agy FROM foglalasok INNER JOIN szobak ON foglalasok.szoba = szobak.szazon INNER JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
+
+app.get("/szobakkihasznaltsaga", (req, res) => {
+    const sql = " SELECT sznev, SUM(fo) AS osszes_vendeg, SUM(DATEDIFF(tav, erk)) AS osszes_ott_toltott_ejszaka FROM foglalasok INNER JOIN szobak ON foglalasok.szoba = szobak.szazon INNER JOIN vendegek ON foglalasok.vendeg = vendegek.vsorsz GROUP BY sznev;";
+    db.query(sql, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result)
+    })
+})  
+
+app.get("/valasztottszoba", (req, res) =>{
+    const sql = " ";
+    db.query(sql, (err, result) => {
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+
 // Szerver indítása a 3001-es porton
     app.listen(3001, () => {
         console.log("Server is running on port 3001");
